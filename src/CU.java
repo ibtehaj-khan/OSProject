8fd70055fb95cd9b1c966061fe15f98e4e785181
Muhammad Ibtehaj Khan <ibtehaj.official@gmail.com>
2021-10-11 14:54:03 +0500

    Change of Param type in Instructions Class

--- src/CU.java
+++ src/CU.java
@@ -10,7 +10,33 @@ public class CU {
         this.INSTRUCTIONS = new Instructions(this.MEMORY,this.REGISTERS);
     }
 
+    //    load file data into memory Array and start from the address
     public void load_program(String filename, int address) throws Exception {
+
+    }
+
+    //    process the opcode to find the type of instruction, then run the instruction
+    public void execute_instruction(char opcode){
+
+    }
+
+    //    Run the No Operand Instructions
+    public void run(char opcode){
+
+    }
+
+    //    Run the Single Operand Instructions
+    public void run(char opcode, char R1){
+
+    }
+
+    //    Run the Register - Register Instructions
+    public void run(char opcode, char R1, char R2){
+
+    }
+
+    //    Run the Register - Immediate and Memory Instructions
+    public void run(char opcode, char R1, char[] imm){
 
     }
 }
--- src/Instructions.java
+++ src/Instructions.java
@@ -11,42 +11,42 @@ public class Instructions {
     //    //////// Register - Register Instructions ////////
 
     //    Copy R2 Content in R1
-    public void MOV(byte R1, byte R2){
+    public void MOV(char R1, char R2){
 
     }
 
     //    Add R2 in R1, and save the value in R1.
     //    Set Flag Register if value overflows
-    public void ADD(byte R1, byte R2){
+    public void ADD(char R1, char R2){
 
     }
 
     //    Subtract R2 from R1, and save the value in R1.
     //    Set Flag Register if value is negative
-    public void SUB(byte R1, byte R2){
+    public void SUB(char R1, char R2){
 
     }
 
     //    Multiply R1 and R2, and save the value in R1.
     //    Set Flag Register if value overflows
-    public void MUL(byte R1, byte R2){
+    public void MUL(char R1, char R2){
 
     }
 
     //    R1 is divided by R2, and save the value in R1.
     //    Throw exception if R2 is zero
-    public void DIV(byte R1, byte R2) throws Exception{
+    public void DIV(char R1, char R2) throws Exception{
 
     }
 
     //    Perform Bitwise Logical AND between R2 and R1, and save the value in R1
-    public void AND(byte R1, byte R2){
+    public void AND(char R1, char R2){
     //    we don't have to convert the byte into bitset, just use bitwise operator
 
     }
 
     //    Perform Bitwise Logical OR between R2 and R1, and save the value in R1
-    public void OR(byte R1, byte R2){
+    public void OR(char R1, char R2){
     //    we don't have to convert the byte into bitset, just use bitwise operator
 
     }
@@ -55,86 +55,86 @@ public class Instructions {
 
     //    Copy imm value in R1.
     //    Handle Exceptions thrown by Convert methods.
-    public void MOVI(byte R1, int imm) throws Exception{
+    public void MOVI(char R1, char[] imm) throws Exception{
     //    Convert the int to byte using Convert.I2B(int value)
     }
 
     //    Add imm value in R1, and save the value in R1.
     //    Set Flag Register if value overflows
     //    Handle Exceptions thrown by Convert methods.
-    public void ADDI(byte R1, int imm) throws Exception{
+    public void ADDI(char R1, char[] imm) throws Exception{
     //    Convert the int to byte using Convert.I2B(int value)
     }
 
     //    Subtract imm value from R1, and save the value in R1.
     //    Set Flag Register if value is negative
     //    Handle Exceptions thrown by Convert methods.
-    public void SUBI(byte R1, int imm){
+    public void SUBI(char R1, char[] imm){
     //    Convert the int to byte using Convert.I2B(int value)
     }
 
     //    Multiply R1 and imm value, and save the value in R1.
     //    Set Flag Register if value overflows
     //    Handle Exceptions thrown by Convert methods.
-    public void MULI(byte R1, int imm){
+    public void MULI(char R1, char[] imm){
     //    Convert the int to byte using Convert.I2B(int value)
     }
 
     //    R1 is divided by imm value, and save the value in R1.
     //    Throw exception if R2 is zero
     //    Handle Exceptions thrown by Convert methods.
-    public void DIVI(byte R1, int imm) throws Exception{
+    public void DIVI(char R1, char[] imm) throws Exception{
     //    Convert the int to byte using Convert.I2B(int value)
     }
 
     //    Perform Bitwise Logical AND between imm value and R1, and save the value in R1
-    public void ANDI(byte R1, int imm){
+    public void ANDI(char R1, char[] imm){
     //    Convert the int to byte using Convert.I2B(int value)
     }
 
     //    Perform Bitwise Logical OR between imm value and R1, and save the value in R1
-    public void ORI(byte R1, int imm){
+    public void ORI(char R1, char[] imm){
     //    Convert the int to byte using Convert.I2B(int value)
     }
 
     //    Check the flag register and jump to the offset
     //    Throws error if base/limit is crossed
-    public void BZ(int imm){
+    public void BZ(char[] imm){
 
     }
 
     //    Check the flag register and jump to the offset
     //    Throws error if base/limit is crossed
-    public void BNZ(int imm){
+    public void BNZ(char[] imm){
 
     }
 
     //    Check the flag register and jump to the offset
     //    Throws error if base/limit is crossed
-    public void BC(int imm){
+    public void BC(char[] imm){
 
     }
 
     //    Check the flag register and jump to the offset
     //    Throws error if base/limit is crossed
-    public void BS(int imm){
+    public void BS(char[] imm){
 
     }
 
     //    Jump to the offset
     //    Throws error if base/limit is crossed
-    public void JMP(int imm){
+    public void JMP(char[] imm){
 
     }
 
     //    Push Program counter on stack, and jump to the offset
     //    Throws error if base/limit is crossed
-    public void CALL(int imm){
+    public void CALL(char[] imm){
 
     }
 
     //    Do the service called by imm
-    public void ACT(int imm){
+    public void ACT(char[] imm){
 
     }
 
@@ -142,25 +142,25 @@ public class Instructions {
 
     //    Load the value from memory present at offset and save that into R1.
     //    Throws error if base/limit is crossed
-    public void MOVL(byte R1, int offset) throws Exception{
+    public void MOVL(char R1, char[] imm) throws Exception{
 
     }
 
     //    store R1 value into the Memory.
     //    Throws error if base/limit is crossed
-    public void MOVS(byte R1, int offset) throws Exception{
+    public void MOVS(char R1, char[] imm) throws Exception{
 
     }
 
     //    //////// Single Operand Instructions ////////
 
     //    Shift R1 value to the Left by 1 bit, and save the value into R1
-    public void SHL(byte R1){
+    public void SHL(char R1){
 
     }
 
     //    Shift R1 value to the Right by 1 bit, and save the value into R1
-    public void SHR(byte R1){
+    public void SHR(char R1){
 
     }
 
@@ -168,7 +168,7 @@ public class Instructions {
     //    Shift R1 value to the Left by 1 bit,
     //    store the left most value in the right most position
     //    save the value into R1
-    public void RTL(byte R1){
+    public void RTL(char R1){
 
     }
 
@@ -176,27 +176,27 @@ public class Instructions {
     //    Shift R1 value to the right by 1 bit,
     //    store the right most value in the left most position
     //    save the value into R1
-    public void RTR(byte R1){
+    public void RTR(char R1){
 
     }
 
     //    Increase the value of R1 by 1, and store the value in R1
-    public void INC(byte R1){
+    public void INC(char R1){
 
     }
 
     //    Decrease the value of R1 by 1, and store the value in R1
-    public void DEC(byte R1){
+    public void DEC(char R1){
 
     }
 
     //    Push the value of R1 on stack
-    public void PUSH(byte R1){
+    public void PUSH(char R1){
 
     }
 
     //    Pop the value from stack, and store the value in R1
-    public void POP(byte R1){
+    public void POP(char R1){
 
     }
 
