44d61cc42771db8aedc254fb40540547b19aa521
Muhammad Ibtehaj Khan <ibtehaj.official@gmail.com>
2021-12-12 00:57:10 +0500

    Memory Manager almost finished

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -1,9 +1,28 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class MemoryManager {
-    private PMMU MemoryUnit;
+    // Table containing Free Frames to provide Pages
+    private LinkedList<Integer> FreeTable;
 
-    public MemoryManager(PMMU memoryUnit) {
-        MemoryUnit = memoryUnit;
+    public MemoryManager() {
+        this.FreeTable = new LinkedList<Integer>();
+        // Initialize FreeTable
+        for(int i = 0; i < 512; i++){
+            FreeTable.add(i);
+        }
+    }
+
+    public int getPage() throws Exception{
+        int page;
+        try{
+           page = FreeTable.removeFirst();
+        } catch (Exception e){
+            throw new Exception("No Free Pages left to allocate");
+        }
+        return page;
+    }
+
+    public void clearPage(int page){
+        FreeTable.addLast(page);
     }
 }
